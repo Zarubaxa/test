476312cfd55464a194b49bfc7e68a632ac778fcb
Anar <zarubaxa@gmail.com>
2019-10-13 15:50:47 +0300

    правки

--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php
 use Slim\Views\PhpRenderer;
-
-include "vendor/autoload.php";
+require 'vendor/autoload.php';
 
 $app = new Slim\App();
 $container = $app->getContainer();
-$container['renderer'] = new PhpRenderer("./templates");
+$container['renderer'] = new PhpRenderer("../templates");
 
-$app->get('/about/{name}', function ($request, $response, $args) {
+$app->get('/about', function ($request, $response, $args) {
     return $this->renderer->render($response, "about.phtml", $args);
 });
 
