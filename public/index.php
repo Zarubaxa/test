925a794f506d557819683f93691090f5d5049715
Anar <zarubaxa@gmail.com>
2019-10-24 23:21:14 +0300

    из

--- public/index.php
+++ public/index.php
@@ -2,12 +2,14 @@
 use Slim\Views\PhpRenderer;
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new Slim\App();
-$container = $app->getContainer();
-$container['renderer'] = new PhpRenderer("../templates");
 
-$app->get('/about', function ($request, $response, $args) {
-    return $this->renderer->render($response, "about.phtml", $args);
+$app = Slim\AppFactor::creates();
+
+$app->get('/about/{name}', function ($request, $response) {
+    $renderer = new PhpRenderer('../templates');
+    return $renderer->render($response, "about.phtml");
 });
 
 $app->run();
+
+
