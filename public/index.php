7bf43c248938d582f53c6c3fea5cf8cda6a63a48
Anar <zarubaxa@gmail.com>
2019-10-29 23:18:32 +0300

    проба2

--- public/index.php
+++ public/index.php
@@ -1,11 +1,9 @@
 <?php
-use Slim\Views\PhpRenderer;
+use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
-
 $app->get('/about/{name}', function ($request, $response, $args) {
     $renderer = new PhpRenderer('../templates');
     return $renderer->render($response, "about.phtml", $args);
